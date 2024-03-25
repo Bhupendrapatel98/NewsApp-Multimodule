04b14b0dc6b37fcae04904be111d5721c0a879d6
bhupendrasmt <bhupendrasmt@gmail.com>
2024-03-25 23:13:04 +0530

    created lottie animation in mainActivity and created BaseApplication

--- app/src/main/java/com/app/newsappapplication/MainActivity.kt
+++ app/src/main/java/com/app/newsappapplication/MainActivity.kt
@@ -2,10 +2,16 @@ package com.app.newsappapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.app.newsappapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
