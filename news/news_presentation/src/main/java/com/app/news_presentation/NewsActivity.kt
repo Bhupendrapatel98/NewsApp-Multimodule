d1ed11a76e099cb83d0cdb92e6c50dfd253272ab
bhupendrasmt <bhupendrasmt@gmail.com>
2024-03-27 11:42:30 +0530

    presentation layer setup and show data in ui done

--- news/news_presentation/src/main/java/com/app/news_presentation/NewsActivity.kt
+++ news/news_presentation/src/main/java/com/app/news_presentation/NewsActivity.kt
@@ -4,8 +4,17 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.app.common_utills.Navigator
+import com.app.news_presentation.databinding.ActivityNewsBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+@AndroidEntryPoint
 class NewsActivity : AppCompatActivity() {
 
     companion object{
@@ -15,9 +24,39 @@ class NewsActivity : AppCompatActivity() {
         }
     }
 
+    lateinit var activityNewsBinding: ActivityNewsBinding
+    private val newsViewModel: NewsViewModel by viewModels()
+    private val newsAdapter = NewsAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+        activityNewsBinding = DataBindingUtil.setContentView(this,R.layout.activity_news)
+
+        initView()
+        setObservers()
+    }
+
+    private fun initView() {
+        activityNewsBinding.rvArticles.adapter = newsAdapter
+    }
+
+    private fun setObservers() {
+        lifecycleScope.launchWhenStarted {
+            newsViewModel.newsArticle.collectLatest {
+                if (it.isLoading) {
+                    activityNewsBinding.progressBar.visibility= View.VISIBLE
+                }
+                if (it.error.isNotBlank()) {
+                    activityNewsBinding.progressBar.visibility= View.GONE
+                    Toast.makeText(this@NewsActivity, it.error, Toast.LENGTH_LONG).show()
+                }
+                it.data?.let {
+                    activityNewsBinding.progressBar.visibility= View.GONE
+                    newsAdapter.setData(it)
+                }
+            }
+        }
+
     }
 }
 
